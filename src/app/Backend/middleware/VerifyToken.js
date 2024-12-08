c974ac5308594289172edd4d4feb63430afee4a5
Raihan <raihansaladin001@gmail.com>
2024-12-08 13:45:55 +0800

    menambahkan role dan name di jwt.verify

--- src/app/Backend/middleware/VerifyToken.js
+++ src/app/Backend/middleware/VerifyToken.js
@@ -6,8 +6,10 @@ export const verifyToken = (req, res, next) => {
     if(token == null) return res.sendStatus(401);
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
         if(err) return res.sendStatus(403);
-        req.email = decoded.email;
         req.userId = decoded.userId;
+        req.name = decoded.name;
+        req.email = decoded.email;
+        req.role = decoded.role;
         next();
     })
 }
